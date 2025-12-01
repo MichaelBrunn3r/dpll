23c3e5899e42aa86fafdee71aacc2835031a5443
Michael Brunner <michaelbrunn3r@gmail.com>
2025-12-01 15:38:14 +0100

    chore: Use harder uf100 problem for benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,7 +6,7 @@ use dpll::dpll::DPLLSolver;
 use dpll::partial_assignment::VarState;
 
 fn bench_parse_dimacs(c: &mut Criterion) {
-    let data = fs::read("./benches/uf20-01.cnf").expect("failed to read fixture");
+    let data = fs::read("./benches/uf100-01.cnf").expect("failed to read fixture");
 
     c.bench_function("parse_dimacs", |b| {
         b.iter(|| {
@@ -16,10 +16,8 @@ fn bench_parse_dimacs(c: &mut Criterion) {
 }
 
 fn bench_solve(c: &mut Criterion) {
-    let data = fs::read("./benches/uf20-01.cnf").expect("failed to read fixture");
+    let data = fs::read("./benches/uf100-01.cnf").expect("failed to read fixture");
     let problem = dpll::parser::parse_dimacs_cnf(&data).expect("failed to parse fixture");
-    assert_eq!(problem.num_vars, 20);
-    assert_eq!(problem.clauses.len(), 91);
     let mut assignment_buffer = vec![VarState::new_unassigned(); problem.num_vars];
 
     c.bench_function("solve", |b| {
