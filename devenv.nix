df8330a5cde566dc77951e6023e0f880dcf4ad2d
Michael Brunner <michaelbrunn3r@gmail.com>
2025-12-12 15:35:31 +0100

    chore: Add cnfgen

--- devenv.nix
+++ devenv.nix
@@ -9,10 +9,23 @@
     git
     perf
     heaptrack
+    (pkgs.python3.withPackages (ps: [
+      ps.networkx
+      ps.pyparsing
+    ]))
   ];
-  languages.rust = {
-    enable = true;
-    channel = "nightly";
-    mold.enable = true;
+  languages = {
+    rust = {
+      enable = true;
+      channel = "nightly";
+      mold.enable = true;
+    };
+    python = {
+      enable = true;
+      venv = {
+        enable = true;
+        requirements = "cnfgen";
+      };
+    };
   };
 }
